143055197b10a049596c6d643775514ae6105092
= <vinhvipvl@gmail.com>
2021-09-16 10:10:43 +0700

    init

---
+++ src/main/java/foodorder/controllers/WelcomeController.java
@@ -0,0 +1,13 @@
+package foodorder.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class WelcomeController {
+
+	@RequestMapping("index")
+	public String showIndex() {
+		return "index";
+	}
+}
