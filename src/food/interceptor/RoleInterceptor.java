e539d4650294e33e1787c6264c485e3cd8e2e3e9
Vinhh <vinhvipvl@gmail.com>
2021-10-28 09:05:03 +0700

    interceptor - chỉ admin mới được truy cập vào admin panel

---
+++ src/food/interceptor/RoleInterceptor.java
@@ -0,0 +1,39 @@
+package food.interceptor;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
+
+import food.entity.Account;
+
+public class RoleInterceptor extends HandlerInterceptorAdapter {
+
+	@Override
+	public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler)
+			throws Exception {
+		HttpSession session = request.getSession();
+		Account user = (Account) session.getAttribute("account");
+		if (user != null && user.getAccountId() == 1)
+			return true;
+
+		response.sendRedirect(request.getContextPath() + "/home.htm");
+		return false;
+	}
+
+	@Override
+	public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler,
+			ModelAndView modelAndView) throws Exception {
+		// TODO Auto-generated method stub
+		super.postHandle(request, response, handler, modelAndView);
+	}
+
+	@Override
+	public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex)
+			throws Exception {
+		// TODO Auto-generated method stub
+		super.afterCompletion(request, response, handler, ex);
+	}
+}
